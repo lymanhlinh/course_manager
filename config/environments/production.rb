2895ec1403c9bc218bc5b2e7eb26fc43478ba780
Lý Mạnh Linh <manhlinh.ctn@gmail.com>
2016-03-06 16:01:00 +0700

    [f] fix host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "www.looneyes-manager.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "looneyes-manager.herokuapp.com" }
 end
